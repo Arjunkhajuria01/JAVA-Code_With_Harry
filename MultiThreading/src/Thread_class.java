20542b98d608faf37e14360ef480e6b26dee2c82
Arjunkhajuria01 <bharatsaida67@gmail.com>
2025-09-06 23:39:52 +0530

    Threads using Thread class

---
+++ MultiThreading/src/Thread_class.java
@@ -0,0 +1,28 @@
+//Multithreading
+// By creating a Thread by extending Thread class.
+
+// 1) creating a class that extends Thread:-
+    class Mythreads extends Thread{
+// 2) Overriding the run method  :-
+    public void run(){
+        while(true) {
+            System.out.println("Im cooking ");
+        }
+    }
+}
+    class Mythreads2 extends Thread{
+    public void run(){
+        while(true) {
+            System.out.println("Im chatting");
+        }
+    }
+    }
+
+    public class Thread_class {
+        public static void main(String[] args) {
+    Mythreads mt = new Mythreads();
+    Mythreads2 mt2 = new Mythreads2();
+    mt.start();
+    mt2.start();
+    }
+}
